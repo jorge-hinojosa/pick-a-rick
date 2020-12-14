aa32128252d6d0a7aad1cac4bdbe653446bcf8e9
Jorge Hinojosa <jorge94@live.com>
2020-12-13 23:58:37 -0600

    added media q to searchgrid

--- src/components/SearchGrid.jsx
+++ src/components/SearchGrid.jsx
@@ -1,9 +1,28 @@
 import React, { useContext, useEffect, Fragment } from 'react';
-import SearchCard from './SearchCard';
-import { ProfileContext } from './ProfilesContextProvider';
+import { Media } from 'react-matches';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_RICKS } from '../queries';
+import { ProfileContext } from './ProfilesContextProvider';
 import { GridSkeleton } from './Skeleton';
+import SearchCard from './SearchCard';
+
+const MEDIA_QUERIES = {
+  xs: {
+    maxWidth: 400,
+  },
+  sm: {
+    minWidth: 401,
+  },
+  md: {
+    minWidth: 700,
+  },
+  lg: {
+    minWidth: 920,
+  },
+  xl: {
+    minWidth: 1200,
+  },
+};
 
 const SearchGrid = () => {
   const res = useQuery(GET_RICKS);
@@ -17,31 +36,41 @@ const SearchGrid = () => {
   }, [res, c.profiles]);
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr 1fr',
-        gridGap: '16px',
-      }}
-    >
-      {c.profiles ? (
-        c.profiles.map((profile) => (
-          <SearchCard
-            key={profile.id}
-            id={profile.id}
-            photoUrl={profile.image}
-            handle={profile.name}
-            location={profile.location?.name}
-          />
-        ))
-      ) : (
-        <Fragment>
-          {Array.from({ length: 20 }, (_, i) => (
-            <GridSkeleton key={i} />
-          ))}
-        </Fragment>
+    <Media queries={MEDIA_QUERIES}>
+      {({ resolve }) => (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: resolve({
+              xs: '1fr',
+              sm: '1fr 1fr',
+              md: '1fr 1fr 1fr',
+              lg: '1fr 1fr 1fr 1fr',
+              xl: '1fr 1fr 1fr 1fr 1fr',
+            }),
+            gridGap: '16px',
+          }}
+        >
+          {c.profiles ? (
+            c.profiles.map((profile) => (
+              <SearchCard
+                key={profile.id}
+                id={profile.id}
+                photoUrl={profile.image}
+                handle={profile.name}
+                location={profile.location?.name}
+              />
+            ))
+          ) : (
+            <Fragment>
+              {Array.from({ length: 20 }, (_, i) => (
+                <GridSkeleton key={i} />
+              ))}
+            </Fragment>
+          )}
+        </div>
       )}
-    </div>
+    </Media>
   );
 };
 
